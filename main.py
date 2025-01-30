bd23296770a55d9789cb02811e446145cfab0be2
natvixen96 <izzy.hemlock@gmail.com>
2025-01-30 16:48:41 -0600

    add word count to python file

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "/home/natvixen96/workplace/github.com/Nat-C96/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
