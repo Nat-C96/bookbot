8d44da1f2f6519f3c22943eb28da9d0ac9d8e485
natvixen96 <izzy.hemlock@gmail.com>
2025-01-31 17:28:42 -0600

    adding character count function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "/home/natvixen96/workplace/github.com/Nat-C96/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letters = get_characters(text)
     print(f"{num_words} words found in the document")
+    print(letters)
 
 
 def get_num_words(text):
@@ -13,6 +15,16 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_characters(text):
+    letters = {}
+    lower_string = text.lower()
+    for letter in lower_string:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
 
 
 main()
